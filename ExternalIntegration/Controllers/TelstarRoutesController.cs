39e20b218ef81f9d19e485952d75fe97d2777387
Nicolai Wiwe Andersen <niwa@netcompany.com>
2021-05-20 10:53:06 +0200

    Remove weather api

---
+++ ExternalIntegration/Controllers/TelstarRoutesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ExternalIntegration.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TelstarRoutesController : ControllerBase
+    {
+        private readonly ILogger<TelstarRoutesController> _logger;
+
+        public TelstarRoutesController(ILogger<TelstarRoutesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<TelstarRoutes> Get() 
+        {
+            var rng = new Random();
+            return Enumerable.Range(0, 4).Select(index => new TelstarRoutes {
+                Price = 20,
+                Time = 20
+            }).ToArray();
+        }
+    }
+}
---
+++ ExternalIntegration/TelstarRoutes.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExternalIntegration
+{
+    public class TelstarRoutes
+    {
+        public int Price { get; set; }
+
+        public int Time { get; set; }
+    }
+}
